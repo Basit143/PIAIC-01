870e48efd003180b51350375f9c8920a7f1c8718
Basit143 <139692375+Basit143@users.noreply.github.com>
2023-07-21 10:46:36 +0500

    First commit

---
+++ index.js
@@ -0,0 +1,74 @@
+"use strict";
+console.log("Hello World i am Abdul Basit");
+let firstName = "Abdul Basit";
+let first1 = "Abbas";
+let first$ = "Ahmad";
+let first_;
+//type annotation 
+let lastName = "Abdul Dayyan";
+let num = 123;
+let isMarried = true;
+let empty = null;
+let unassigned;
+var myName = "Abdul Basit";
+var hello = "Hello" + myName;
+console.log(hello); //HELLO Abdul Basit
+let testVariables = 1;
+console.log(testVariables);
+let n1 = 1;
+let n2 = 2;
+console.log(n1 + n2); // ?
+let strl = "1";
+let str2 = "2";
+console.log(strl + str2); // ?
+let n3 = 5;
+let n4 = 2;
+console.log(n3 - n4); // ?
+let n5 = 5;
+let n6 = 2;
+console.log(n5 * n6); //?
+let n7 = 11;
+let n8 = 2;
+console.log(n7 / n8); //?
+let n9 = 24;
+let n10 = 7;
+console.log(n9 ** n10); //?
+let n11 = 79;
+let n12 = 4;
+console.log(n11 % n12); //?
+let n = 5;
+console.log(n); //5
+n += 5;
+console.log(n); //10
+n -= 5;
+console.log(n); //5
+n *= 5;
+console.log(n); //25
+n /= 5;
+console.log(n); //10
+n %= 5;
+n /= 5;
+console.log(n); //5
+n **= 5;
+console.log(n); //125
+n %= 5;
+console.log(n); //5
+n **= 5;
+console.log(n); //125
+n %= 5;
+console.log(n); //5
+let m = 5;
+console.log(m == 5); //
+console.log(m === 5); //
+console.log(m != 5); //
+console.log(m > 8); //
+console.log(m < 8); //
+console.log(m >= 8); //
+console.log(m <= 8); //
+let o = 5;
+console.log(o >= 5 && o < 10); //
+console.log(o >= 5 && o < 10); //
+console.log(o >= 5 || o < 10); //
+console.log(o > 5 || o < 10); //
+console.log(!(o < 10)); //
+console.log(!(o > 10)); //
